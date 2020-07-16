9227cb4e1b767436a3f81a532d3d7dfe2d8cce67
matt <matthewbizley@gmail.com>
2020-07-16 22:15:20 +0100

    Initial Commit

---
+++ Timers/TimersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimersApp.swift
+//  Timers
+//
+//  Created by Matt on 16/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct TimersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
